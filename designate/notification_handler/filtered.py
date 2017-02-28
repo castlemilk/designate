e5866aaf6037cf976785cf62b96167bfef905917
Ben Ebsworth <ben.ebsworth@gmail.com>
2017-03-01 10:04:56 +1100

    working notification_filter for specific ip address range

--- designate/notification_handler/filtered.py
+++ designate/notification_handler/filtered.py
@@ -59,23 +59,27 @@ class FilteredHandler(NotificationHandler):
         ]
 
     def process_notification(self, context, event_type, payload):
-        LOG.debug('NovaFixedFilteredHandler received notification - %s', event_type)
+        LOG.debug('FilteredHandler received notification - %s', event_type)
         context = DesignateContext().elevated()
         context.all_tenants = True
 
         zone_id = cfg.CONF[self.name].zone_id
         zone_name = cfg.CONF[self.name].zone_name
+        LOG.debug('FilteredHandler zone_id - %s', zone_id)
+        LOG.debug('FilteredHandler zone_name - %s', zone_name)
         if event_type == 'compute.instance.create.end':
             valid_address = lambda x: IP(x) in IP(cfg.CONF[self.name].address_filter)
             payload['project'] = getattr(context, 'tenant', None)
             record_name = '%s.%s' % (payload['host'], zone_name)
+            LOG.debug('FilteredHandler record_name - %s', record_name)
             filtered_addresses = []
             for address in payload['fixed_ips']:
                 if valid_address(address['address']):
                     filtered_addresses.append(address)
 
             if filtered_addresses:
-                LOG.debug('NovaFixedFilteredHandler Filtered %d -> %d', len(payload['fixed_ips']))
+                LOG.debug('FilteredHandler Before Filtered %d', len(payload['fixed_ips']))
+                LOG.debug('FilteredHandler After Filtered %d', len(filtered_addresses))
                 for address in filtered_addresses:
                     recordset_values = {
                         'zone_id': zone_id,
@@ -85,13 +89,14 @@ class FilteredHandler(NotificationHandler):
                     record_values = {
                         'data': address['address'], 
                     }
+                    LOG.debug('FilteredHandler:recordset_values:zone_id:', recordset_values['zone_id'])
+                    LOG.debug('FilteredHandler:recordset_values:record_name:', recordset_values['name'])
+                    LOG.debug('FilteredHandler:recordset_values:type:', recordset_values['type'])
+                    LOG.debug('FilteredHandler:record_values:data:', record_values['data'])
                     recordset = self._find_or_create_recordset(context, **recordset_values)
+
+                    LOG.debug('FilteredHandler:recordset:id:', recordset['id'])
                     self.central_api.create_record(context, zone_id, recordset['id'], Record(**record_values))
-                self._create(addresses=filtered_addresses,
-                         extra=payload,
-                         zone_id=zone_id,
-                         resource_id=payload['instance_id'],
-                         resource_type='instance')
             else:
                 LOG.debug('FilteredHandler No Results after filtering for %s', self.address_filter)
 
