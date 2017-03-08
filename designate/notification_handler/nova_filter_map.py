18173c632f84c3585461d4c59de6ed789bd9bb26
Ben Ebsworth <ben.ebsworth@gmail.com>
2017-03-08 23:12:51 +1100

    added generic filter map, covers all dns requirements

--- designate/notification_handler/nova_filter_map.py
+++ designate/notification_handler/nova_filter_map.py
@@ -23,25 +23,24 @@ from designate.notification_handler.base import BaseAddressHandler
 LOG = logging.getLogger(__name__)
 
 cfg.CONF.register_group(cfg.OptGroup(
-    name='handler:nova_fixed_filtered',
-    title="Configuration for Nova Notification Handler WITH FILTERING"
+    name='handler:nova_filter_map',
+    title="Configuration for Nova Notification Handler WITH MAPPED FILTERING"
 ))
 cfg.CONF.register_opts([
     cfg.ListOpt('notification-topics', default=['notifications']),
+    cfg.ListOpt('zone_items'),
     cfg.StrOpt('control-exchange', default='nova'),
-    cfg.StrOpt('address-filter', default='0.0.0.0/0'),
-    cfg.StrOpt('zone-id'),
     cfg.MultiStrOpt('formatv4'),
     cfg.MultiStrOpt('format', deprecated_for_removal=True,
                     deprecated_reason="Replaced by 'formatv4/formatv6'",
                     ),
     cfg.MultiStrOpt('formatv6')
-], group='handler:nova_fixed_filtered')
+], group='handler:nova_filter_map')
 
 
-class NovaFixedFilterHandler(BaseAddressHandler):
-    """Handler for Nova's notifications WITH FILTERING"""
-    __plugin_name__ = 'nova_fixed_filtered'
+class NovaFilterMapHandler(BaseAddressHandler):
+    """Handler for Nova's notifications WITH MAPPED FILTERING"""
+    __plugin_name__ = 'nova_filter_map'
 
     def get_exchange_topics(self):
         exchange = cfg.CONF[self.name].control_exchange
@@ -57,31 +56,36 @@ class NovaFixedFilterHandler(BaseAddressHandler):
         ]
 
     def _get_ip_data(self, addr_dict):
-        data = super(NovaFixedFilterHandler, self)._get_ip_data(addr_dict)
+        data = super(NovaFilterMapHandler, self)._get_ip_data(addr_dict)
         data['label'] = addr_dict['label']
         return data
 
     def process_notification(self, context, event_type, payload):
-        LOG.debug('NovaFixedFilterHandler received notification - %s', event_type)
-
-        zone_id = cfg.CONF[self.name].zone_id
+        """
+        Assume we have an item list: zone_id:zone_name:address_filter, ...
+        i.e 13413525-242424-23423424:example.com:192.168.0.0/24, ...
+        """
+        LOG.debug('NovaFilterMapHandler received notification - %s', event_type)
+        valid_address = lambda x, y: IP(x) in IP(y)
+        map_items = cfg.CONF[self.name].zone_items
+        zone_map = map(lambda x: {'zone_id': x[0], 'zone_name': x[1], 'address_filter': x[2]}, [x.strip().split(':') for x in map_items])
         if event_type == 'compute.instance.create.end':
             payload['project'] = getattr(context, 'tenant', None)
-            valid_address = lambda x: IP(x) in IP(cfg.CONF[self.name].address_filter)
-            filtered_addresses = []
-            for address in payload['fixed_ips']:
-                LOG.debug('NovaFixedFilterHandler:address:%s', address['address'])
-                if valid_address(address['address']):
-                    LOG.debug('NovaFixedFilterHandler:address:valid:True')
-                    filtered_addresses.append(address) 
-            if filtered_addresses:
-                self._create(addresses=filtered_addresses,
-                         extra=payload,
-                         zone_id=zone_id,
-                         resource_id=payload['instance_id'],
-                         resource_type='instance')
+            for zone in zone_map:
+                for address in payload['fixed_ips']:
+                    LOG.debug('NovaFixedFilterHandler:address_filter:%s', zone['address_filter'])
+                    LOG.debug('NovaFilterMapHandler:address:%s', address['address'])
+                    if valid_address(address['address'], zone['address_filter']):
+                        LOG.debug('NovaFixedFilterHandler:address:valid:True')
+                        LOG.debug('NovaFixedFilterHandler:address:adding to domain:%s', zone['zone_name'])
+                        self._create(addresses=[address],
+                                     extra=payload,
+                                     zone_id=zone['zone_id'],
+                                     resource_id=payload['instance_id'],
+                                     resource_type='instance')
 
         elif event_type == 'compute.instance.delete.start':
-            self._delete(zone_id=zone_id,
+            for zone in zone_map:
+                self._delete(zone_id=zone['zone_id'],
                          resource_id=payload['instance_id'],
                          resource_type='instance')
